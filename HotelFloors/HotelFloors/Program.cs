6020f99d4d3cab519161aece329646abc469dc44
donFranko99 <franciszek.rybski@microsoft.wsei.edu.pl>
2021-05-14 00:09:14 +0200

    first ideas

--- HotelFloors/HotelFloors/Program.cs
+++ HotelFloors/HotelFloors/Program.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HotelFloors
 {
+    class Coordinate
+    {
+        public int x, y;
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
     class Program
     {
+        public void lookForRooms(int p, int q, bool[] available)
+        {
+
+        }
         static void Main(string[] args)
         {
             int numberOfIterations = int.Parse(Console.ReadLine());
@@ -13,8 +27,11 @@ namespace HotelFloors
                 int[] data = Array.ConvertAll<string, int>(input.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
                 int m = data[0];
                 int n = data[1];
+                bool inRoom = false;
+                bool closedRoom = false;
                 int numberOfPeople = 0;
                 int numberOfRooms = 0;
+                List<Coordinate> currentRoom = new List<Coordinate>();
                 double avgNumberOfPeople;
 
                 char[,] hotel = new char[m,n];
@@ -27,6 +44,31 @@ namespace HotelFloors
                         hotel[x, y] = tmp[y];
                     }
                 }
+
+                bool[,] visited = new bool[m, n];
+                for (int k = 1; k < m-1; k++)
+                {
+                    for (int j = 1; j < n-1; j++)
+                    {
+                        visited[m, n] = false;
+                    }
+                }
+
+                for (int x = 1; x < m-1; x++)
+                {
+                    for (int y = 1; y < n-1; y++)
+                    {
+                        if (hotel[x,y]=='*' || hotel[x,y]=='_')
+                        {
+                            if (!inRoom)
+                            {
+                                inRoom = true;
+                                numberOfRooms++;
+                            }
+                        }
+                    }
+                }
+
                 foreach (char item in hotel)
                 {
                     if (item == '*')
